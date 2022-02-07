b52d97a5491fab6f3b14f07962ebb00691a499f8
Viiz23 <97685553+Viiz23@users.noreply.github.com>
2022-02-07 21:51:33 +0530

    Add files via upload

--- flask/app.py
+++ flask/app.py
@@ -7,7 +7,6 @@ import os
 
 TEMPLATE_DIR = os.path.abspath('C:/Users/vinun/OneDrive/Desktop/DesktopImp/BackupProject/flask/templates')
 STATIC_DIR = os.path.abspath('C:/Users/vinun/OneDrive/Desktop/DesktopImp/BackupProject/flask/static')
-
 # app = Flask(__name__) # to make the app run without any
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
@@ -32,28 +31,37 @@ def home():
 def book():
     return render_template("book.html")          #Moving forward code
 
-@app.route("/pathology", methods=['POST','GET'])
-def pathology():
+@app.route("/appointment", methods=['POST','GET'])
+def appointment():
     if request.method == 'POST':
         # Fetch form data
         userDetails = request.form.get
         Name = userDetails('full_name')
         Gender = userDetails('radio')
-        AadharNumber = userDetails('aadhar')
-        Address = userDetails('address')
         PhoneNumber = userDetails('phone')
-        Pincode = userDetails('pincode')
-        Fever = userDetails('fever')
-        LossOfSmellandTaste = userDetails('lossofsmelltaste')
-        Other = userDetails('other')
-        MedicalHistory = userDetails('medical')
-        AppointmentDate = userDetails('appointmentdate')
+        Dateofbirth = userDetails('dob')
+        Department = userDetails('dept')
+        FirstVisit = userDetails('visit')
+        Appointment_DT = userDetails('appointment_time')
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(Name, Gender, AadharNumber, Address, PhoneNumber, Pincode, Fever, LossOfSmellandTaste, Other, MedicalHistory, AppointmentDate) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(Name, Gender, AadharNumber, Address, PhoneNumber, Pincode, Fever, LossOfSmellandTaste, Other, MedicalHistory, AppointmentDate))
+        cur.execute("INSERT INTO medforms(Name, Gender,PhoneNumber,Dateofbirth,Department,FirstVisit,Appointment_DT) VALUES(%s,%s,%s,%s,%s,%s,%s)",(Name, Gender,PhoneNumber,Dateofbirth,Department,FirstVisit,Appointment_DT))
         mysql.connection.commit()
         cur.close()
-        return redirect('/user')
-    return render_template("pathology.html")
+        return redirect('/medform')
+    return render_template("appointment.html")
+
+
+@app.route('/medform')
+def medform():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM medforms")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template('medform.html',userDetails=userDetails)
+
+
+
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
